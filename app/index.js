bb57402c057a21f9333e1a23024f4c3cc27d84fb
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-09-28 03:42:14 +0000

    app is using api routes

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import config from './config.js';
+import apiRoutes from './routes/index.js';
 
 const app = new express();
+app.use(`/api`, apiRoutes);
+
+
 
 app.listen(config.port, () => {
 	console.log(`Server running on port: ${config.port}`);
