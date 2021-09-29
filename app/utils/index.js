420057355ba298083839f2ea6934986bfe604967
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-09-29 03:55:47 +0000

    setup export froms

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,1 +1,6 @@
-//IMPORT FILES
+export {default as getUser} from "./getUser.js";
+export {default as getRepoList} from "./getRepoList.js";
+export {default as getListOfRepoNames} from "./getListOfRepoNames.js";
+export {default as getRepoStats} from "./getRepoStats.js";
+export {default as authenticateUser} from "./authenticateUser.js";
+ 
